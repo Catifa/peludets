65e5e7871a0200c327a9fb31858c49b01b89073d
Catifa <akantorz92@gmail.com>
2020-12-24 20:08:02 +0100

    Añadido mapa explorador

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,15 @@ import VueRouter from 'vue-router';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 import {routes} from './routes';
+import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
+
+Vue.component('l-map', LMap);
+Vue.component('l-title-layer', LTileLayer);
+Vue.component('l-maker', LMarker);
 
 const router = new VueRouter({
     mode: 'history',
