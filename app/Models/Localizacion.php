a42dcb57c4fef70dd3ba5489d9fd82936bed89e9
Catifa <akantorz92@gmail.com>
2021-01-17 20:03:18 +0100

    Modelo, Controlador y Migracion Localizacion

---
+++ app/Models/Localizacion.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Localizacion extends Model
+{
+    public $table = "sitios_interes";
+    use HasFactory;
+}
