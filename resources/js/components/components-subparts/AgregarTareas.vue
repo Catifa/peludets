3394e3c61433fde5b9cb7624a2ad9e402500f02e
Ailoji <mailoji20@gmail.com>
2021-01-22 01:02:05 +0100

    drag and drop tareas

--- resources/js/components/components-subparts/AgregarTareas.vue
+++ resources/js/components/components-subparts/AgregarTareas.vue
@@ -1,87 +1,84 @@
 
 
 <template>
+  <div id="app" class="container mt-5">
+    <h3>{{ titulo }}</h3>
 
-<div id = "app" class="container mt-5">
+    <input
+      type="text"
+      class="form-control my-3"
+      name=""
+      id=""
+      v-model="nuevaTarea"
+      v-on:keyup.enter="agregarTarea"
+    />
 
- 
-            <h3>{{titulo}}</h3>
+    <button class="btn btn-primary" @click="agregarTarea">Agregar</button>
 
-      <input
-        type="text"
-        class="form-control my-3"
-        name=""
-        id=""
-        v-model="nuevaTarea"
-        v-on:keyup.enter="agregarTarea"
-      />
-
-       <button class="btn btn-primary" @click="agregarTarea">Agregar</button>
-
-      <div class="mt-3" v-for="(t,index) of tareas" :key="t.nombre">
-        <div role="alert" :class="['alert' , t.estado ? 'alert-success' : 'alert-danger']">
-          <div class="d-flex justify-content-between align-items-center">
-              <div>
-                 {{index+1}}.  {{t.nombre}}
-              </div>
-            <div>
-              <button class="btn btn-success btn-sm" @click="editarTarea(index)">Agregar</button>
-              <button class="btn btn-danger btn-sm" @click="eliminarTarea(index)">Eliminar</button>
-            </div>
+    <drag class="mt-3" v-for="(t, index) of tareas" :key="t.nombre">
+      <div
+        role="alert"
+        :class="['alert', t.estado ? 'alert-success' : 'alert-danger']"
+      >
+        <div class="d-flex justify-content-between align-items-center">
+          <div>{{ index + 1 }}. {{ t.nombre }}</div>
+          <div>
+            <button class="btn btn-success btn-sm" @click="editarTarea(index)">
+              Agregar
+            </button>
+            <button class="btn btn-danger btn-sm" @click="eliminarTarea(index)">
+              Eliminar
+            </button>
           </div>
         </div>
-
-    
-  
-   
+      </div>
+    </drag>
   </div>
-</div> 
 </template>
 
 <script>
+import { Drag } from "vue-drag-drop";
+
 export default {
-    name:'AgregarTareas',
-       data() {
-        return { 
-         titulo:"Tareas",
-        tareas: [],
-        nuevaTarea: '',
-        } 
-
+  components: {
+    drag: Drag,
+  },
+  name: "AgregarTareas",
+  data() {
+    return {
+      titulo: "Tareas",
+      tareas: [],
+      nuevaTarea: "",
+    };
+  },
+  methods: {
+    agregarTarea() {
+      console.log(this.nuevaTarea);
+      this.tareas.push({
+        nombre: this.nuevaTarea,
+        estado: false,
+      });
+      console.log(this.tareas);
+      this.nuevaTarea = "";
+      localStorage.setItem("peludets-vue", JSON.stringify(this.tareas));
     },
-    methods: {
-        agregarTarea(){
-            console.log(this.nuevaTarea);
-            this.tareas.push({
-                nombre: this.nuevaTarea,
-                estado: false
-            });
-            console.log(this.tareas)
-            this.nuevaTarea= '';
-            localStorage.setItem('peludets-vue',JSON.stringify(this.tareas));
-
-        },
-        editarTarea(index){
-           this.tareas[index].estado=true;
-           localStorage.setItem('peludets-vue',JSON.stringify(this.tareas));
-
-        },
-        eliminarTarea(index){
-           this.tareas.splice(index,1)
-           localStorage.setItem('peludets-vue',JSON.stringify(this.tareas));
-
-        }
+    editarTarea(index) {
+      this.tareas[index].estado = true;
+      localStorage.setItem("peludets-vue", JSON.stringify(this.tareas));
     },
-    created: function () {
-        let datosDB = JSON.parse(localStorage.getItem('peludets-vue'));
-        if (datosDB ===null) {
-            this.tareas = [];
-        } else {
-            this.tareas= datosDB;
-        }
-
-        
+    eliminarTarea(index) {
+      this.tareas.splice(index, 1);
+      localStorage.setItem("peludets-vue", JSON.stringify(this.tareas));
+    },
+  },
+  created: function () {
+    let datosDB = JSON.parse(localStorage.getItem("peludets-vue"));
+    if (datosDB === null) {
+      this.tareas = [];
+    } else {
+      this.tareas = datosDB;
     }
-}
+  },
+};
 </script>
 
