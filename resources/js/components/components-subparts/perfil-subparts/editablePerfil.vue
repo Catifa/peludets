306ca03a073085738bc30a0c155dbb9f1ac59ff1
Catifa <akantorz92@gmail.com>
2021-02-01 19:36:31 +0100

    Intento 1 de PerfilEditable

--- resources/js/components/components-subparts/perfil-subparts/editablePerfil.vue
+++ resources/js/components/components-subparts/perfil-subparts/editablePerfil.vue
@@ -1,36 +1,70 @@
 <template>
   <div class="col-md-11 mt-1">
-    <form @submit.prevent="saveTextProf" method="post">
+    <form>
       <editor
-        v-model="textoEditable"
+        v-model="textoEditable.val"
         api-key="6pjhqn98u0f2a21lhh3fnygr2np8uwshnw0huk04p8dty974"
         :init="{
-            height: 500,
-            languaje: 'es',
-            menubar: false,
-            plugins: [
-                'advlist lists textcolor colorpicker emoticons image preview save table wordcount'
-            ],
-            toolbar:
-                'undo redo | formatselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | \
-                bullist numlist table outdent indent | removeformat | emoticons image | preview save'
-            }"
+          height: 500,
+          languaje: 'es',
+          menubar: false,
+          plugins: [
+            'advlist lists textcolor colorpicker emoticons image preview save table wordcount',
+          ],
+          toolbar:
+            'undo redo | formatselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | \
+                bullist numlist table outdent indent | removeformat | emoticons image | preview save',
+        }"
+        v-bind:initial-value="'' + texto.texto + ''"
       ></editor>
     </form>
+    <button @click="showEditor = !showEditor" class="btn btn-verde-peludets">
+      Editar Perfil
+    </button>
   </div>
 </template>
 
 <script>
+
+import Editor from "@tinymce/tinymce-vue";
+
 export default {
+  components: {
+    editor: Editor,
+  },
   data() {
     return {
-      textoEditable: {}
+      textoEditable: {},
+      texto: {},
+      usuario: {},
+      showEditor: false,
     };
   },
   methods: {
-    saveTextProf() {
-      console.log(this.textoEditable);
-    }
-  }
+    saveContent() {
+      let obj = {
+        val: this.textoEditable.val,
+        id: this.usuario.id,
+      };
+
+      this.axios.post("api/usuario/setProfText", obj);
+
+      this.$router.go();
+    },
+  },
+  mounted() {
+    this.usuario.id = this.$root.user.id;
+    this.axios
+      .post("api/usuario/getProfText", this.usuario)
+      .then((response) => {
+        // Parseo de la respuesta de la base de datos a JSON
+        let textoSinParsear = response.data[0].textoPerfil;
+        let textoToJson = JSON.parse(
+          textoSinParsear.replace(/(\w+)=/g, '"$1":')
+        );
+        this.texto = textoToJson;
+        $('#perfEditable').html(this.texto.texto);
+      });
+  },
 };
 </script>
