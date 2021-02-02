0f4c0330de122a26a4a96fb5a708bcf37564441e
erick93-bit <aciegoe@gmail.com>
2021-02-02 16:56:43 +0100

    chat

--- resources/js/appChat.js
+++ resources/js/appChat.js
@@ -6,9 +6,20 @@ const express = require('express');
 
 const appChat = express();
 
-appChat.get("/", () => {
-  resizeBy.send('Hola');
+appChat.use(express.static(__dirname + 'resources/js/components/Profesionales.vue'));
 
-});
+const http = require('http').createServer(appChat);
+
+const socket = require('./socket');
+socket(http);
 
 module.exports = appChat;
+
+
+
+new Vue({
+    el: '#app',
+    data: {
+      nick: 'Erick'
+    }
+});
