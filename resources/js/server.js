8b7198260da3ea56b5008c959ef898a6b27e853c
Catifa <akantorz92@gmail.com>
2021-02-11 15:50:42 +0100

    Cosas del chat pa fuera

--- resources/js/server.js
+++
@@ -1,7 +0,0 @@
-const http = require('C:/xampp/htdocs/peludets/resources/js/server.js');
-
-const port = 3000;
-
-http.listen(port, () => {
-    //console.log('el servidor esta corriendo por el puerto', port);
-});
