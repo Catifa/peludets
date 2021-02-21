2a9de21832dacd5da9619dce614641f67a7b720c
Catifa <akantorz92@gmail.com>
2021-02-21 21:15:03 +0100

    Metodo getAll en route de Api

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -6,29 +6,49 @@
   <div>
     <div class="row">
       <!-- Carousel -->
-      <div id="carouselCentral" class="carousel slide col-md-8 mt-3" data-ride="carousel">
+      <div
+        id="carouselCentral"
+        class="carousel slide col-md-8 mt-3"
+        data-ride="carousel"
+      >
         <ol class="carousel-indicators">
-          <li data-target="#carouselCentral" data-slide-to="0" class="active"></li>
+          <li
+            data-target="#carouselCentral"
+            data-slide-to="0"
+            class="active"
+          ></li>
           <li data-target="#carouselCentral" data-slide-to="1"></li>
           <li data-target="#carouselCentral" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="../../img/carousel/slide1.jpg" class="d-block w-100" alt="..." />
+            <img
+              src="../../img/carousel/slide1.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
             <div class="carousel-caption d-none d-md-block">
               <h5>{{ $t("home.sliderTitulo1") }}</h5>
               <p>{{ $t("home.sliderDesc1") }}</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img src="../../img/carousel/slide2.jpg" class="d-block w-100" alt="..." />
+            <img
+              src="../../img/carousel/slide2.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
             <div class="carousel-caption d-none d-md-block">
               <h5>{{ $t("home.sliderTitulo2") }}</h5>
               <p>{{ $t("home.sliderDesc2") }}</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img src="../../img/carousel/slide3.jpg" class="d-block w-100" alt="..." />
+            <img
+              src="../../img/carousel/slide3.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
             <div class="carousel-caption d-none d-md-block">
               <h5>{{ $t("home.sliderTitulo3") }}</h5>
               <p>{{ $t("home.sliderDesc3") }}</p>
@@ -40,9 +60,11 @@
       <div class="col-md-3 col-sd-10 mt-4 mx-auto align-self-center">
         <form id="formTrabajos" class="rounded">
           <div class="form-group">
-            <label class="mt-2x" for="exampleFormControlSelect1">{{ $t("home.formTrabajos") }}</label>
-            <select class="form-control" id="exampleFormControlSelect1" v-model="profesion">
-              <option v-for="profesion in profesiones" :key="profesion.id">{{ profesion.nombre_profesion }}</option>
+            <label class="mt-2x">{{ $t("home.formTrabajos") }}</label>
+            <select class="form-control" v-model="profesion">
+              <option v-for="profesion in profesiones" :key="profesion.id">
+                {{ profesion.nombre_profesion }}
+              </option>
             </select>
           </div>
           <button
@@ -50,7 +72,9 @@
             type="submit"
             class="btn btn-lila-peludets"
             @click.prevent="buscarTrabajos"
-          >{{ $t('home.formBotonBuscar') }}</button>
+          >
+            {{ $t("home.formBotonBuscar") }}
+          </button>
         </form>
       </div>
     </div>
@@ -65,7 +89,7 @@
             width="100%"
           />
           <div class="card-body bg-crema-peludets">
-            <p class="card-text">{{ $t('home.cardBox1') }}</p>
+            <p class="card-text">{{ $t("home.cardBox1") }}</p>
           </div>
         </div>
       </div>
@@ -78,7 +102,7 @@
             width="100%"
           />
           <div class="card-body bg-crema-peludets">
-            <p class="card-text">{{ $t('home.cardBox2') }}</p>
+            <p class="card-text">{{ $t("home.cardBox2") }}</p>
           </div>
         </div>
       </div>
@@ -91,7 +115,7 @@
             width="100%"
           />
           <div class="card-body bg-crema-peludets">
-            <p class="card-text">{{ $t('home.cardBox3') }}</p>
+            <p class="card-text">{{ $t("home.cardBox3") }}</p>
           </div>
         </div>
       </div>
@@ -104,7 +128,7 @@
             width="100%"
           />
           <div class="card-body bg-crema-peludets">
-            <p class="card-text">{{ $t('home.cardBox4') }}</p>
+            <p class="card-text">{{ $t("home.cardBox4") }}</p>
           </div>
         </div>
       </div>
@@ -116,19 +140,19 @@ export default {
   data() {
     return {
       profesiones: [],
-      profesion: {}
+      profesion: {},
     };
   },
   methods: {
     buscarTrabajos() {
-      let prof = this.profesion; 
-      this.$router.push({name: 'profesionales', params: {prof}});
-    }
+      let prof = this.profesion;
+      this.$router.push({ name: "profesionales", params: { prof } });
+    },
   },
   created() {
-    this.axios.get("api/profesiones").then(response => {
+    this.axios.get("api/profesiones/getAll").then((response) => {
       this.profesiones = response.data;
     });
-  }
+  },
 };
 </script>
