d1859e4e53c34a5f8a987480b0ed07a8f2966b82
AlxisRadio <adriansanchezlara@gmail.com>
2021-02-22 15:59:25 +0100

    buscador

---
+++ database/migrations/2021_02_21_151252_chats.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Chats extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->integer('idUserOne');
+            $table->integer('idUserTwo');
+            $table->array('mensajes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -15,12 +15,14 @@
   transition: all 400ms ease;
 }
 
-.col-md-3, .col-md-4, .col-md-5, .col-md-6 {
+.col-md-3,
+.col-md-4,
+.col-md-5,
+.col-md-6 {
   flex-grow: 1;
   transition: all 400ms ease;
 }
-
-select.oferta{
+.profesionales {
   display: none;
 }
 </style>
@@ -30,35 +32,63 @@ select.oferta{
     <div class="row">
       <div id="buscador" class="navbar col-md-2 container">
         <div class="row mx-auto">
-          <select  class="oferta form-select col-md-7">
-            <option disabled selected>Categoria</option>
-            <option>Oferta</option>
-            <option>Profesionales</option>
-            <option>Sitios de interes</option>
-          </select>
-          <div class="form-select col-md-7 mr-auto row">
+          <div id="category" class="col-md-7 mr-auto row">
             <p class="mb-0">Categoria</p>
-          <select v-model="category" class="form-select" @change="hola()">
-            <option>Oferta</option>
-            <option>Profesionales</option>
-            <option>Sitios de interes</option>
-          </select>
+            <select v-model="category" class="form-select" @change="menu()">
+              <option>Oferta</option>
+              <option>Profesionales</option>
+              <option>Sitios de interes</option>
+            </select>
           </div>
-          <button class="btn col-md-4 ml-auto">Search</button>
+          <div class="profesionales col-md-3 mr-auto row">
+            <p class="mb-0">Tipo Prof</p>
+            <select v-model="tipoProf" class="form-select">
+              <option>Oferta</option>
+              <option>Profesionales</option>
+              <option>Sitios de interes</option>
+            </select>
+          </div>
+          <div class="profesionales col-md-3 mr-1 row">
+            <p class="mb-0">Disponibilidad</p>
+            <select v-model="disponibilidadProf" class="form-select">
+              <option>Oferta</option>
+              <option>Profesionales</option>
+              <option>Sitios de interes</option>
+            </select>
+          </div>
+          <div id="submit" class="btn col-md-3 disabled">Search</div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+
 export default {
   methods: {
-    hola() {
+    menu() {
       if (this.category == "Oferta") {
-      console.log("hola");
+        this.hideAll();
+        document.getElementById("submit").className =
+          "btn col-md-3";
+        document.getElementById("buscador").className =
+          "navbar col-md-3 container";
+        $(".oferta").show();
       }
-      $("#buscador").toggleClass("col-md-3 col-md-2");
-      $(".oferta").show();
+      if (this.category == "Profesionales") {
+        this.hideAll();
+        document.getElementById("submit").className =
+          "btn col-md-3";
+        document.getElementById("buscador").className =
+          "navbar col-md-4 container";
+        document.getElementById("category").className =
+          "col-md-3 mr-auto row";
+        $(".profesionales").show();
+      }
+    },
+    hideAll() {
+      $(".oferta").hide();
+      $(".profesionales").hide();
     },
   },
 };
