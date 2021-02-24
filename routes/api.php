50946a054f25476a8c481c343c239d419ba3cab5
erick93-bit <aciegoe@gmail.com>
2021-02-24 19:58:29 +0100

    Mail

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,9 @@ use App\Http\Controllers\MascotasController;
 use App\Http\Controllers\MensajesController;
 use App\Http\Controllers\OfertasController;
 use App\Http\Controllers\SitiosInteresController;
+use App\Mail\ContactanosMailable;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -90,4 +91,28 @@ Route::group(['prefix' => 'explorador'], function () {
 Route::group(['prefix' => 'chat'], function(){
     Route::post('insert',[MensajesController::class,'insert']);
     Route::post('select',[MensajesController::class,'select']);
+});
+
+//Mail 
+Route::get('contactanos', function() {
+    $correo = [
+        'title' => 'Tienes una nueva solicitud',
+        'body' => 'PAYASO'
+    ];
+    Mail::to('aciegoe@gmail.com')->send(new \App\Mail\ContactanosMailable($correo));
+  
+
+    return 'Correo enviado';
+
+});
+
+Route::get('solicitud', function() {
+    $correo = [
+        'title' => 'Tienes una nueva solicitud',
+        'body' => 'PAYASO'
+    ];
+    Mail::to('aciegoe@gmail.com')->send(new \App\Mail\solicitud($correo));
+
+    return 'Correo enviado';
+
 });
