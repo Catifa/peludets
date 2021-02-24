a08ac361ce104ff6a0fae8958ccd416a3d8f83e2
AlxisRadio <adriansanchezlara@gmail.com>
2021-02-24 18:33:03 +0100

    .

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,5 +1,5 @@
 <style>
-.navbar {
+#buscador {
   border-bottom: solid 1px #ccc;
 }
 
@@ -22,7 +22,9 @@
   flex-grow: 1;
   transition: all 400ms ease;
 }
-.profesionales {
+.profesionales,
+.sitios,
+.ofertas {
   display: none;
 }
 </style>
@@ -31,29 +33,53 @@
   <div class="cotainer-fluid">
     <div class="row">
       <div id="buscador" class="navbar col-md-2 container">
-        <div class="row mx-auto">
-          <div id="category" class="col-md-7 mr-auto row">
+        <div class="row col-md-12">
+          <div id="category" class="col-md-7 mx-auto row">
             <p class="mb-0">Categoria</p>
-            <select v-model="category" class="form-select" @change="menu()">
+            <select
+              v-model="category"
+              class="form-select px-0 col-md-12"
+              @change="menu()"
+            >
               <option>Oferta</option>
               <option>Profesionales</option>
               <option>Sitios de interes</option>
             </select>
           </div>
-          <div class="profesionales col-md-3 mr-auto row">
+          <div class="profesionales col-md-3 mx-auto row">
             <p class="mb-0">Tipo Prof</p>
-            <select v-model="tipoProf" class="form-select">
+            <select v-model="tipoProf" class="form-select px-0 col-md-12">
               <option>Oferta</option>
               <option>Profesionales</option>
               <option>Sitios de interes</option>
             </select>
           </div>
-          <div class="profesionales col-md-3 mr-1 row">
+          <div class="profesionales col-md-3 mx-auto row">
             <p class="mb-0">Disponibilidad</p>
-            <select v-model="disponibilidadProf" class="form-select">
-              <option>Oferta</option>
-              <option>Profesionales</option>
-              <option>Sitios de interes</option>
+            <select
+              v-model="disponibilidadProf"
+              class="form-select px-0 col-md-12"
+            >
+              <option>Telematico</option>
+              <option>Presencial</option>
+            </select>
+          </div>
+          <div class="sitios col-md-3 mx-auto row">
+            <p class="mb-0">Tipo</p>
+            <select v-model="tipoInteres" class="form-select px-0 col-md-12">
+              <option>Tienda</option>
+              <option>Parque</option>
+            </select>
+          </div>
+          <div class="sitios col-md-3 mx-auto row">
+            <p class="mb-0">Localización</p>
+            <select
+              v-model="localizacionInteres"
+              class="form-select px-0 col-md-12"
+            >
+              <option>España</option>
+              <option>Mi España</option>
+              <option>La España</option>
             </select>
           </div>
           <div id="submit" class="btn col-md-3 disabled">Search</div>
@@ -63,31 +89,37 @@
   </div>
 </template>
 <script>
-
 export default {
   methods: {
     menu() {
-      if (this.category == "Oferta") {
-        this.hideAll();
-        document.getElementById("submit").className =
-          "btn col-md-3";
-        document.getElementById("buscador").className =
-          "navbar col-md-3 container";
-        $(".oferta").show();
+      switch (this.category) {
+        case "Sitios de interes":
+          this.sitios();
+          break;
+        case "Profesionales":
+          this.profesionales();
+          break;
       }
-      if (this.category == "Profesionales") {
-        this.hideAll();
-        document.getElementById("submit").className =
-          "btn col-md-3";
-        document.getElementById("buscador").className =
-          "navbar col-md-4 container";
-        document.getElementById("category").className =
-          "col-md-3 mr-auto row";
-        $(".profesionales").show();
-      }
+    },
+    sitios() {
+      this.hideAll();
+      document.getElementById("submit").className = "btn col-md-2";
+      document.getElementById("buscador").className =
+        "navbar col-md-4 container";
+      document.getElementById("category").className = "col-md-3 mx-auto row";
+      $(".sitios").show();
+    },
+    profesionales() {
+      this.hideAll();
+      document.getElementById("submit").className = "btn col-md-2";
+      document.getElementById("buscador").className =
+        "navbar col-md-4 container";
+      document.getElementById("category").className = "col-md-3 mx-auto row";
+      $(".profesionales").show();
     },
     hideAll() {
       $(".oferta").hide();
+      $(".sitios").hide();
       $(".profesionales").hide();
     },
   },
