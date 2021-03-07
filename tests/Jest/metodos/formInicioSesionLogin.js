6402053536e3de867157ea2c8b97bbbce4edd13f
Ailoji <mailoji20@gmail.com>
2021-03-07 11:52:53 +0100

    jest

---
+++ tests/Jest/metodos/formInicioSesionLogin.js
@@ -0,0 +1,18 @@
+loading = false;
+llamadaLogin = {
+    id: 1
+};
+user = {
+    id: 1
+};
+
+function login() {
+    if ((llamadaLogin = user)) {
+        "Registro completado", "Bienvenido, ", "success";
+        this.loading = true;
+    } else {
+        console.log(error);
+        this.loading = false;
+    }
+}
+module.exports = login;
