74534eb01a5f6df8d296a437ee450a6c82378928
Catifa <akantorz92@gmail.com>
2021-03-14 16:46:58 +0100

    Arreglados fallos de consola

--- resources/js/components/components-subparts/Perfil_Modal_Update_User.vue
+++ resources/js/components/components-subparts/Perfil_Modal_Update_User.vue
@@ -18,80 +18,48 @@
         </div>
         <!-- Body Modal -->
         <div class="modal-body">
-          <form action method="post" enctype="multipart/form-data">
-            <!-- Nombre -->
-            <div class="mb-3">
-              <label>
-                {{ $t("perfil.perfilEditable.modalUpdateUser.nombreModal") }}
-              </label>
-              <input
-                type="text"
-                class="form-control"
-                v-model="user.name"
-                :placeholder="$root.user.name"
-              />
-            </div>
-            <!-- Apellido -->
-            <div class="mb-3">
-              <label>
-                {{ $t("perfil.perfilEditable.modalUpdateUser.apellidoModal") }}
-              </label>
-              <input
-                type="text"
-                class="form-control"
-                v-model="user.lastname"
-                :placeholder="$root.user.lastname"
-              />
-            </div>
-            <!-- Segundo Apellido -->
-            <div class="mb-3">
-              <label for="apellido">
-                {{ $t("perfil.perfilEditable.modalUpdateUser.segundoApellidoModal") }}
-              </label>
-              <input
-                type="text"
-                class="form-control"
-                v-model="user.secondlastname"
-                :placeholder="$root.user.secondlastname"
-              />
-            </div>
-            <!-- Correo -->
-            <div class="mb-3">
-              <label>
-                {{ $t("perfil.perfilEditable.modalUpdateUser.correoModal") }}
-              </label>
-              <input
-                type="text"
-                class="form-control"
-                v-model="user.email"
-                :placeholder="$root.user.email"
-              />
-            </div>
-            <!-- DNI -->
-            <div class="mb-3">
-              <label>
-                DNI
-              </label>
-              <input
-                type="text"
-                class="form-control"
-                v-model="user.dni"
-                :placeholder="$root.user.dni"
-              />
-            </div>
-            <!-- Foto -->
-            <div id="divFileMascota" class="mb-3 btn btn-azul-peludets">
-              <label>
-                 {{ $t("perfil.perfilEditable.modalUpdateUser.fotoModal") }}
-              </label>
-              <input
-                type="file"
-                class="form-control-file"
-                id="file"
-                @change="updateImg($event)"
-              />
-            </div>
-          </form>
+          <!-- Nombre -->
+          <div class="mb-3">
+            <label>
+              {{ $t("perfil.perfilEditable.modalUpdateUser.nombreModal") }}
+            </label>
+            <input type="text" class="form-control" v-model="userModal.name" />
+          </div>
+          <!-- Apellido -->
+          <div class="mb-3">
+            <label>
+              {{ $t("perfil.perfilEditable.modalUpdateUser.apellidoModal") }}
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="userModal.lastname"
+            />
+          </div>
+          <!-- Correo -->
+          <div class="mb-3">
+            <label>
+              {{ $t("perfil.perfilEditable.modalUpdateUser.correoModal") }}
+            </label>
+            <input type="text" class="form-control" v-model="userModal.email" />
+          </div>
+          <!-- DNI -->
+          <div class="mb-3">
+            <label> DNI </label>
+            <input type="text" class="form-control" v-model="userModal.dni" />
+          </div>
+          <!-- Foto -->
+          <div id="divFileMascota" class="mb-3">
+            <label>
+              {{ $t("perfil.perfilEditable.modalUpdateUser.fotoModal") }}
+            </label>
+            <input
+              type="file"
+              class="form-control-file"
+              id="file"
+              @change="updateImg($event)"
+            />
+          </div>
         </div>
         <!-- Footer Modal -->
         <div class="modal-footer bg-azul-peludets">
@@ -104,11 +72,7 @@
             {{ $t("perfil.perfilEditable.modalUpdateUser.guardarModal") }}
           </button>
           <!-- Cerrar -->
-          <button
-            type="button"
-            class="btn btn-danger"
-            data-dismiss="modal"
-          >
+          <button type="button" class="btn btn-danger" data-dismiss="modal">
             {{ $t("perfil.perfilEditable.modalUpdateUser.cerrarModal") }}
           </button>
         </div>
@@ -118,44 +82,74 @@
 </template>
 
 <script>
-
 import Swal from "sweetalert2";
-import Api from '../../Api';
-import { utils } from '../../utils';
+import Api from "../../Api";
 
 export default {
-  props: ["updateUser"],
+  props: {
+    propHideModal: { type: Function },
+  },
   data() {
     return {
-      user: undefined,
-      img: {},
+      user: {},
+      userModal: {},
+      img: undefined,
     };
   },
   methods: {
     recuperarUser() {
-      Api().get("/user").then((response) => {
-        this.user = response.data;
-        this.$root.user = response.data;
-        this.$root.photo = response.data.photo;
-        console.log(response.data);
-      });
+      Api()
+        .get("/user")
+        .then((response) => {
+          this.user = response.data;
+          this.userModal = this.user;
+          this.$root.user = response.data;
+          this.$root.photo = response.data.photo;
+        });
     },
 
     updateUsuario(id) {
-      this.user.userId = this.$root.user.id;
-      this.user.img = this.img;
-      this.user.id = id;
+      this.userModal.userId = this.$root.user.id;
+      this.userModal.img = this.img;
+      this.userModal.id = id;
+
       Api()
-        .post("/usuario/updateUsuario", this.user)
+        .post("/usuario/updateUsuario", this.userModal)
         .then(() => {
-          Swal.fire("Usuario modificada", "success");
-          this.recuperarUser()
+          Swal.fire("Usuario modificado correctamente", "", "success");
+          this.recuperarUser();
+          this.propHideModal();
         });
     },
 
     updateImg(e) {
-      utils.imgUpload(e);
-    }
-  }
+      var formData = new FormData();
+      var file = e.target.files[0];
+      formData.append("Filedata", file);
+      var t = file.type.split("/").pop().toLowerCase();
+      if (t != "jpeg" && t != "jpg" && t != "png") {
+        alert("Seleccione un formato de imágen válido: (jpeg, jpg, png)");
+        document.getElementById("file").value = "";
+        return false;
+      }
+      if (file.size > 1024000) {
+        alert("Tamaño de imágen máximo (1MB)");
+        document.getElementById("file").value = "";
+        return false;
+      }
+
+      let f = e.target.files[0];
+      let reader = new FileReader();
+
+      reader.onloadend = () => {
+        this.img = reader.result;
+      };
+
+      reader.readAsDataURL(f);
+    },
+  },
+  mounted() {
+    this.recuperarUser();
+  },
 };
 </script>
