11571f335f5d4e107868970ae752a06f2083e7dd
Catifa <akantorz92@gmail.com>
2021-03-14 22:05:21 +0100

    Division de mascotas

--- resources/js/components/Perfil.vue
+++ resources/js/components/Perfil.vue
@@ -60,9 +60,9 @@
             role="tab"
             aria-controls="tareas"
             aria-selected="false"
-            >
+          >
             {{ $t("perfil.submenuTareas") }}
-            </a>
+          </a>
         </li>
       </ul>
       <!-- Zona renderizado submenu -->
@@ -85,148 +85,7 @@
           role="tabpanel"
           aria-labelledby="mascota-tab"
         >
-          <div class="row mt-5">
-            <button
-              type="button"
-              class="btn btn-azul-peludets ml-5"
-              data-toggle="modal"
-              data-target="#form-registroMascota"
-            >
-              Añade tu mascota
-            </button>
-          </div>
-
-          <div class="row">
-            <div
-              class="col-md-3 mt-3"
-              v-for="mascota in mascotas"
-              :key="mascota.id"
-            >
-              <div class="card" style="width: 18rem" name="mascotas">
-                <img
-                  v-bind:src="mascota.photo"
-                  class="card-img-top"
-                  width="286px"
-                  height="286px"
-                  alt="..."
-                />
-                <div class="card-body bg-crema-peludets">
-                  <h5 class="card-title">{{ mascota.nombre }}</h5>
-                  <ul>
-                    <li>{{ mascota.especie }}</li>
-                    <li>{{ mascota.raza }}</li>
-                    <li>{{ mascota.edad }}</li>
-                    <li>{{ mascota.peso }}</li>
-                  </ul>
-                  <a
-                    href="javascript:void(0)"
-                    id=""
-                    class="fas fa-cog"
-                    aria-hidden="true"
-                    data-toggle="modal"
-                    data-target="#form-updateMascota"
-                    @click="modalMascota(mascota)"
-                  ></a>
-                  <a
-                    href="javascript:void(0)"
-                    id=""
-                    class="fas fa-trash ml-3"
-                    aria-hidden="true"
-                    @click="deleteMascota(mascota.id)"
-                  ></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <!-- Modal -->
-          <div id="form-registroMascota" class="modal fade" role="dialog">
-            <div class="modal-dialog modal-dialog-centered">
-              <!-- Modal content-->
-              <div class="modal-content">
-                <div class="modal-header">
-                  <div class="container-fluid">
-                    <div class="col-md-12">
-                      <h4 class="modal-title">Añade a tu Mascota</h4>
-                    </div>
-                  </div>
-                </div>
-                <div class="modal-body">
-                  <form action method="post" enctype="multipart/form-data">
-                    <div class="mb-3">
-                      <label for class="form-label">Nombre</label>
-                      <input
-                        type="text"
-                        v-model="mascota.nombre"
-                        class="form-control"
-                        aria-describedby="emailHelp"
-                      />
-                    </div>
-                    <div class="mb-3">
-                      <label for class="form-label">Especie</label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        v-model="mascota.especie"
-                        aria-describedby="emailHelp"
-                      />
-                    </div>
-                    <div class="mb-3">
-                      <label for class="form-label">Raza</label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        v-model="mascota.raza"
-                        aria-describedby="emailHelp"
-                      />
-                    </div>
-                    <div class="mb-3">
-                      <label for class="form-label">Edad</label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        v-model="mascota.edad"
-                        aria-describedby="emailHelp"
-                      />
-                    </div>
-                    <div class="mb-3">
-                      <label for class="form-label">Peso</label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        v-model="mascota.peso"
-                        aria-describedby="emailHelp"
-                      />
-                    </div>
-
-                    <div id="divFileMascota" class="mb-3 btn btn-azul-peludets">
-                      <label id="textoFile" for
-                        >Añade la foto de tu mascota</label
-                      >
-                      <input type="file" id="file" @change="file($event)" />
-                    </div>
-                  </form>
-                </div>
-                <div class="modal-footer">
-                  <button
-                    id="btn-registroMascota"
-                    @click="registerMascota()"
-                    class="btn btn-azul-peludets"
-                  >
-                    Registrala!
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <!-- Modal -->
-          <modalUpdateMascota
-            :propMascota="mascota"
-            id="modalMascota"
-          ></modalUpdateMascota>
+          <perfil-mascotas :propMascotas="mascotas"></perfil-mascotas>
         </div>
         <!-- Zona renderizado Valoraciones -->
         <div
@@ -295,26 +154,22 @@
 </template>
 
 <script>
-
 import Swal from "sweetalert2";
 import Perfil_PerfilEditable from "./components-subparts/Perfil_PerfilEditable.vue";
+import Perfil_Mascotas from "./components-subparts/Perfil_Mascotas.vue";
 import Tareas from "./Tareas";
 import DatosUsuario from "./components-subparts/Perfil_DatosUsuario.vue";
-import Perfil_Modal_Update_Mascotas from "./components-subparts/Perfil_Modal_Update_Mascotas";
 import Api from "../Api";
 
 export default {
   components: {
-    'perfil-editable': Perfil_PerfilEditable,
+    "perfil-editable": Perfil_PerfilEditable,
+    "perfil-mascotas": Perfil_Mascotas,
     Tareas,
-    'datos-usuario': DatosUsuario,
-    modalUpdateMascota: Perfil_Modal_Update_Mascotas,
+    "datos-usuario": DatosUsuario,
   },
-  name: "panel",
   data() {
     return {
-      mascota: {},
-      img: {},
       mascotas: [],
       valoraciones: [],
     };
@@ -330,66 +185,12 @@ export default {
         });
     },
 
-    modalMascota(mascota) {
-      this.mascota = mascota;
-    },
     recuperarMascota() {
       Api()
         .post("/mascota/recuperarMascota")
         .then((response) => {
           this.mascotas = response.data;
-          console.log(response.data);
         });
-    },
-
-    deleteMascota(id) {
-      console.log(id);
-      Api()
-        .post("/mascota/deleteMascota", { id })
-        .then((response) => {
-          Swal.fire("Mascota eliminada", "success");
-          this.recuperarMascota();
-        })
-        .catch((error) => console.log(error))
-        .finally(() => (this.loading = false));
-    },
-    registerMascota() {
-      this.mascota.userId = this.$root.user.id;
-      this.mascota.img = this.img;
-      Api()
-        .post("/mascota/registerMascota", this.mascota)
-        .then((response) => {
-          $("#form-registroMascota").modal("hide");
-          Swal.fire("Registro completado", "success");
-          this.recuperarMascota();
-        })
-        .catch((error) => console.log(error))
-        .finally(() => (this.loading = false));
-    },
-    file(e) {
-      var formData = new FormData();
-      var file = e.target.files[0];
-      formData.append("Filedata", file);
-      var t = file.type.split("/").pop().toLowerCase();
-      if (t != "jpeg" && t != "jpg" && t != "png") {
-        alert("Seleccione un formato de imágen válido: (jpeg, jpg, png)");
-        document.getElementById("file").value = "";
-        return false;
-      }
-      if (file.size > 1024000) {
-        alert("Tamaño de imágen máximo (1MB)");
-        document.getElementById("file").value = "";
-        return false;
-      }
-
-      let f = e.target.files[0];
-      let reader = new FileReader();
-
-      reader.onloadend = () => {
-        this.img = reader.result;
-      };
-
-      reader.readAsDataURL(f);
     },
   },
 
