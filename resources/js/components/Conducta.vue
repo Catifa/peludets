d4a61d563ccee66944d1202acb87ff9bfe32255a
Ailoji <mailoji20@gmail.com>
2021-03-20 23:38:20 +0100

    codigo de conducta

--- resources/js/components/Conducta.vue
+++ resources/js/components/Conducta.vue
@@ -12,44 +12,77 @@
       <ul class="list-group">
         <!--Introduccio-->
         <li class="list-group-item">
-          
+          <p>
+            Integritat: Coherència en les accions, valors, mètodes, mesures i
+            principis. Actuació amb cura i diligència, presa de decisions
+            honestes, justes, imparcials i oportunes, considerant tota la
+            informació rellevant.
+          </p>
         </li>
 
-        <!--1-->
-        <li class="list-group-item">
-         
-        </li>
+
         <!--2-->
         <li class="list-group-item">
-          
+          <p>
+            Igualtat de tracte i no-discriminació: Tothom té dret a no ser
+            discriminat per raó de raça, color, sexe, llengua, religió, opinió
+            política o de qualsevol altra mena, origen nacional o social,
+            fortuna, naixement o altra condició. Discriminar vol dir donar a una
+            persona o a un grup de persones un tracte diferent i desfavorable
+            respecte d’altres, en funció d’un o més trets o característiques o
+            per la seva pertinença a un grup determinat. La discriminació es
+            considera una vulneració dels drets fonamentals i té per
+            conseqüència privar certes persones dels mateixos drets i
+            oportunitats que gaudeix el conjunt de la societat.
+          </p>
         </li>
         <!--3-->
         <li class="list-group-item">
-        
+          <p>
+            Independència i neutralitat: Considerar l’impacte dels comentaris i
+            les accions que un duu a terme, i gestionar la pròpia conducta per
+            tal de representar tots els ciutadans i no pas determinats
+            col·lectius o interessos polítics.
+          </p>
         </li>
         <!--4-->
         <li class="list-group-item">
-        
+          <p>
+            Rendició de comptes: Fer servir els recursos de l’ens de manera
+            responsable i sensata i de manera que s’asseguri un ús eficient,
+            efectiu i apropiat dels recursos humans, naturals, financers i
+            físics, de la propietat i de la informació.
+          </p>
         </li>
-        <!--5-->
-        <li class="list-group-item">
-        
-        </li>
+
         <!--6-->
         <li class="list-group-item">
-        
+          <p>
+            Accessibilitat: Propietat referida a les interfícies d’usuari que en
+            determina la facilitat d’accés independentment de les capacitats
+            físiques, psíquiques o tècniques dels usuaris.
+          </p>
         </li>
         <!--7-->
-        <li class="list-group-item">
-      
-        </li>
+
         <!--8-->
         <li class="list-group-item">
-      
+          <p>
+            Eficàcia i eficiència: El concepte d’eficàcia s’ocupa de la relació
+            entre els resultats perseguits i els obtinguts dels programes o
+            altres activitats. El concepte d’eficiència es refereix a la relació
+            entre la producció de béns, serveis o altres resultats i els
+            recursos utilitzats per produir-los.
+          </p>
         </li>
         <!--9-->
         <li class="list-group-item">
-         
+          <p>
+            Cultura ètica: És la cultura de l’organització informada per la
+            integritat, la responsabilitat, la transparència i el respecte. Una
+            cultura ètica de l’organització es basa en la comprensió comuna dels
+            valors ètics que la caracteritzen.
+          </p>
         </li>
       </ul>
     </div>
