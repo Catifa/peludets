e08a19b1589f7bca3e1c912477d88d63f6247ac6
Ailoji <mailoji20@gmail.com>
2021-04-23 00:49:34 +0200

    canvas paseo de perros

--- resources/js/canvas.js
+++ resources/js/canvas.js
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" value="{{ csrf_token() }}" />
-    <title>Peludets!</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/leaflet.css" />
-
-</head>
-
-<body>
-    <div id="app"></div>
-    <script src="https://kit.fontawesome.com/215bc124ef.js" crossorigin="anonymous"></script>
-    <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/leaflet.js"></script>
-    <script src="https://www.mapquestapi.com/sdk/leaflet/v2.2/mq-map.js?key=S8d7L47mdyAG5nHG09dUnSPJjreUVPeC"></script>
-    <script src="https://www.mapquestapi.com/sdk/leaflet/v2.2/mq-routing.js?key=S8d7L47mdyAG5nHG09dUnSPJjreUVPeC">
-    </script>
-
-    <script>
-        // default map layer
+// default map layer
 let map = L.map("map", {
     layers: MQ.mapLayer(),
     center: [41.50546, 2.11775],
@@ -100,7 +75,6 @@ function submitForm(event) {
     // getting form data
     start = document.getElementById("start").value;
     end = document.getElementById("destination").value;
-    
 
     // run directions function
     runDirection(start, end);
@@ -114,11 +88,3 @@ const form = document.getElementById("form");
 
 // call the submitForm() function when submitting the form
 form.addEventListener("submit", submitForm);
-
-    </script>
-
-
-
-</body>
-
-</html>
--- resources/js/components/Canvas.vue
+++ resources/js/components/Canvas.vue
@@ -1,10 +1,85 @@
 <style>
-@import "../../css/home.css";
+@import "../../css/canvas.css";
 </style>
 
 <template>
-  <div class="container mt-5">
+  <div class="container-fluid mt-5">
+    <!--  <button type="button" class="btn btn-primary" @click="pintarCanvas">
+      Pintar
+    </button>
+   <mapa-exp></mapa-exp>
+    <canvas id="canvas" style="background: white" width="800" height="600">
+    </canvas>-->
+    <div class="col-md-12">
+      <form id="form">
+        <input
+          type="hidden"
+          name="start"
+          class="input"
+          id="start"
+          value="mallorca  badia del valles"
+        />
+        <input
+          type="hidden"
+          name="end"
+          class="input"
+          id="destination"
+          value="ibiza  vadia del valles"
+        />
+        <button class="btn btn-lila-peludets mt-2" type="submit">
+          Paseo 1
+        </button>
+      </form>
 
-<canvas id="c"></canvas>
+      <div id="map" class="col-md-12 mt-5"></div>
+    </div>
   </div>
 </template>
+
+
+<script>
+import MapaExplorador from "./components-subparts/Explorador_mapa.vue";
+
+export default {
+  components: {
+    "mapa-exp": MapaExplorador,
+  },
+  data() {},
+
+  methods: {
+    pintarCanvas() {
+      const ALTURA_CANVAS = 600,
+        ANCHURA_CANVAS = 800;
+
+      var min = 1;
+      var max = 600;
+      var num1 = Math.random() * (max - min) + min;
+      var num2 = Math.random() * (max - min) + min;
+      var num3 = Math.random() * (max - min) + min;
+      var num4 = Math.random() * (max - min) + min;
+      // Obtener el elemento del DOM
+      const canvas = document.querySelector("#canvas");
+      canvas.width = ANCHURA_CANVAS;
+      canvas.height = ALTURA_CANVAS;
+      // Del canvas, obtener el contexto para poder dibujar
+      const contexto = canvas.getContext("2d");
+      // Comenzar
+      contexto.beginPath();
+      // Grosor de línea
+      contexto.lineWidth = 3;
+      // Color de línea
+      contexto.strokeStyle = "blue";
+      // Comenzamos en
+      contexto.moveTo(num1, num2);
+      // Hacemos una línea hasta
+      contexto.lineTo(num3, num4);
+      contexto.stroke(); // "Guardar" cambios
+    },
+  },
+  mounted() {
+    // Recuperar Mascotas
+  },
+};
+</script>
+
+
