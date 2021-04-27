b35257bcc0d13967308308ecd5dc0278a501ecad
Catifa <akantorz92@gmail.com>
2021-04-27 16:28:56 +0200

    Responsive imagenes

--- resources/js/components/components-subparts/Perfil_DatosUsuario.vue
+++ resources/js/components/components-subparts/Perfil_DatosUsuario.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="mb-2">
-    <img
-     v-bind:src="this.$root.photo"
-      class="rounded-circle mt-5"
-      width="300px"
-      height="300px"
-    />
-    <h3 class="mt-5">{{ this.$root.user.name }}</h3>
-    <h3 class="mt-5">{{ this.$root.user.email }}</h3>
+  <div class="row">
+    <div class="col-lg-12">
+      <!-- Imagen -->
+      <img
+        v-bind:src="this.$root.photo"
+        class="rounded img-fluid mt-5"
+        width="450px"
+      />
+      <!-- Nombre -->
+      <h3 class="lila-peludets mt-2">
+        {{ this.$root.user.name }} {{ this.$root.user.lastname }}
+      </h3>
+      <!-- Correo -->
+      <h3 class="lila-peludets">{{ this.$root.user.email }}</h3>
+    </div>
   </div>
 </template>
 <script>
--- resources/js/components/templates/tmp_perfil.vue
+++ resources/js/components/templates/tmp_perfil.vue
@@ -15,17 +15,16 @@
     <div class="col-xs-12 col-md-7">
       <div class="row">
         <!-- Imagen -->
-        <div class="col-xs-6 col-lg-3 mt-5 mr-auto text-center">
+        <div class="col-xs-6 col-lg-4 mt-5 mr-auto text-center">
           <img
             v-bind:src="user.photo"
             class="rounded"
             alt="Imagen usuario"
-            width="200px"
-            height="200px"
+            width="300px"
           />
         </div>
         <!-- Datos Personales -->
-        <div class="col-xs-6 col-lg-9 mt-2">
+        <div class="col-xs-6 col-lg-8 mt-2">
           <!-- Nombre -->
           <div class="row">
             <div class="col-lg-12">
