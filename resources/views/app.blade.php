130da5e2a4ff70e38d836285b83949ccfc65ab56
Ailoji <mailoji20@gmail.com>
2021-05-02 14:01:23 +0200

    app.blade. quitando links de api de mapas

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,6 @@
     <title>Peludets!</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/leaflet.css" />
 
 </head>
 
@@ -17,10 +16,7 @@
     <div id="app"></div>
     <script src="https://kit.fontawesome.com/215bc124ef.js" crossorigin="anonymous"></script>
     <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/leaflet.js"></script>
-    <script src="https://www.mapquestapi.com/sdk/leaflet/v2.2/mq-map.js?key=S8d7L47mdyAG5nHG09dUnSPJjreUVPeC"></script>
-    <script src="https://www.mapquestapi.com/sdk/leaflet/v2.2/mq-routing.js?key=S8d7L47mdyAG5nHG09dUnSPJjreUVPeC">
-    </script>
+
 </body>
 
 </html>
